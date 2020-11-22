2fd359fb66ffa8e2e41b86dc00869a1e1c64d314
mojahige <mojahige@gmail.com>
2020-11-22 23:53:21 +0900

    [server] remove unnecessary code

--- packages/server/src/heros/heros.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { HerosService } from './heros.service';
-import { HerosController } from './heros.controller';
-
-@Module({
-  providers: [HerosService],
-  controllers: [HerosController]
-})
-export class HerosModule {}
