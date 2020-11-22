62b1517f1f6b8585919924f52f2625027918705e
mojahige <mojahige@gmail.com>
2020-11-23 00:13:53 +0900

    [server] add Swagger

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,10 +1,26 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+
+function buildSwagger(app: INestApplication) {
+  const options = new DocumentBuilder()
+    .setTitle('Example')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('doc', app, document);
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
+
+  buildSwagger(app);
 
   await app.listen(3000);
 }
