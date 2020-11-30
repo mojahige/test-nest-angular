a0d0c1ac3ebe52eb80d0d51c8a1c640273aa626f
mojahige <mojahige@gmail.com>
2020-11-30 22:53:06 +0900

    update hero

--- packages/server/src/hero/hero.controller.ts
+++ packages/server/src/hero/hero.controller.ts
@@ -1,18 +1,60 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Controller,
+  Body,
+  Param,
+  Get,
+  Post,
+  Put,
+  Delete,
+} from '@nestjs/common';
 import { HeroService } from './hero.service';
-import { Hero as HeroModel } from '@prisma/client';
+import { Prisma, Hero } from '@prisma/client';
+import { ApiProperty, ApiBody } from '@nestjs/swagger';
+
+class Create implements Prisma.HeroCreateInput {
+  @ApiProperty()
+  name: string;
+}
+
+class Update implements Prisma.HeroUpdateInput {
+  @ApiProperty()
+  name: string;
+}
 
 @Controller('hero')
 export class HeroController {
   constructor(private readonly heroService: HeroService) {}
 
   @Get()
-  getHeros(): Promise<HeroModel[]> {
+  getHeros(): Promise<Hero[]> {
     return this.heroService.heros();
   }
 
   @Get(':id')
-  getHero(@Param('id') id: string): Promise<HeroModel> {
+  getHero(@Param('id') id: string): Promise<Hero> {
     return this.heroService.hero({ id: Number(id) });
+  }
+
+  @Post()
+  @ApiBody({ type: Create })
+  async createHero(@Body() params: Prisma.HeroCreateInput): Promise<Hero> {
+    return this.heroService.createUser(params);
+  }
+
+  @Put(':id')
+  @ApiBody({ type: Update })
+  updateHero(
+    @Param('id') id: string,
+    @Body() data: Prisma.HeroUpdateInput,
+  ): Promise<Hero> {
+    return this.heroService.updateUser({
+      where: { id: Number(id) },
+      data,
+    });
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string): Promise<Hero> {
+    return this.heroService.deleteUser({ id: Number(id) });
   }
 }
