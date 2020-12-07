eceea224351d7cb47872714ec9c81ff8ce67169e
mojahige <mojahige@gmail.com>
2020-12-07 20:31:09 +0900

    update output directory

--- openapi-gen.sh
+++ openapi-gen.sh
@@ -1,4 +1,4 @@
 docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli generate \
     -i /local/packages/server/swagger-spec.json \
     -g typescript-angular \
-    -o /local/packages/client/api-spec
+    -o /local/packages/client/src/api
